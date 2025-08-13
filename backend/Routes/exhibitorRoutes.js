474f64524b44fb554bac2f76606c70a284baf8e8
niha-siddiqui <138647909+niha-siddiqui@users.noreply.github.com>
2025-08-13 10:58:12 +0500

    new

--- backend/Routes/exhibitorRoutes.js
+++ backend/Routes/exhibitorRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const exhibitorController = require("../Controllers/exhibitorController");
 
 router.post("/exhibitorreg", exhibitorController.registerExhibitor);
-router.post("/exhibitorlogin", exhibitorController.loginExhibitor);
+router.post("/exhibitorlogin/", exhibitorController.loginExhibitor);
 router.get("/booking", exhibitorController.getAllExhibitors);
 router.get("/exhibitordashboard", exhibitorController.getAllExhibitors);
 router.get("/exhibitorprofile", exhibitorController.getAllExhibitors);
@@ -12,5 +12,6 @@ router.post("/:id/status", exhibitorController.updateExhibitorStatus);
 router.get("/", exhibitorController.getAllExhibitors);
 
 router.put("/:id/status", exhibitorController.updateExhibitorStatus);
+
 
 module.exports = router;
